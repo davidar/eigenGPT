50f173dcda29fbdef9618bf4cf3e6e0a9e89b3d0
David A Roberts <d@vidr.cc>
2023-05-25 21:47:16 +1000

    test model loading

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-#include <Eigen/Dense>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+
+#include <Eigen/Dense>
+#include <spdlog/spdlog.h>
+
+#include "safetensors.hpp"
+
+namespace fs = std::filesystem;
 
 using Eigen::MatrixXd;
 
@@ -10,4 +18,17 @@ int main() {
   m(0, 1) = -1;
   m(1, 1) = m(1, 0) + m(0, 1);
   std::cout << m << std::endl;
+
+  fs::path path = "../model.safetensors";
+  if (!fs::exists(path))
+    throw std::runtime_error("File does not exist");
+  std::ifstream model(path, std::ios::binary);
+  auto tensors = huggingface::safetensors::deserialize(model);
+  for (auto &[name, meta] : tensors.meta) {
+    spdlog::info("{}: {} {}", name, json(meta.dtype).dump(), json(meta.shape).dump());
+  }
+  float *b_ln = (float*) tensors["ln_f.bias"].data();
+  for (int i = 0; i < 768; i++) {
+    std::cout << b_ln[i] << ",";
+  }
 }
