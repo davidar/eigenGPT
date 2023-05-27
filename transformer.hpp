96290308a9f33ac596a683293df4148763db7398
David A Roberts <d@vidr.cc>
2023-05-27 09:00:42 +0000

    transpose matrices

--- transformer.hpp
+++ transformer.hpp
@@ -20,41 +20,39 @@ class TransformerBlock {
 public:
   Matrix<float, Dynamic, 2 * n_embd> kv;
 
-  Matrix<float, n_embd, 3 * n_embd> w_attn1;
+  Matrix<float, 3 * n_embd, n_embd> w_attn1;
   Vector<float, 3 * n_embd> b_attn1;
 
   Matrix<float, n_embd, n_embd> w_attn2;
   Vector<float, n_embd> b_attn2;
 
-  Matrix<float, n_embd, 4 * n_embd> w_mlp1;
+  Matrix<float, 4 * n_embd, n_embd> w_mlp1;
   Vector<float, 4 * n_embd> b_mlp1;
 
-  Matrix<float, 4 * n_embd, n_embd> w_mlp2;
+  Matrix<float, n_embd, 4 * n_embd> w_mlp2;
   Vector<float, n_embd> b_mlp2;
 
   TransformerBlock(safetensors::safetensors_t param, int b)
       : kv(0, 2 * n_embd),
-        w_attn1(param.matrix(format("h.{}.attn.c_attn.weight", b))),
+        w_attn1(param.matrix(format("h.{}.attn.c_attn.weight", b)).transpose()),
         b_attn1(param.vector(format("h.{}.attn.c_attn.bias", b))),
-        w_attn2(param.matrix(format("h.{}.attn.c_proj.weight", b))),
+        w_attn2(param.matrix(format("h.{}.attn.c_proj.weight", b)).transpose()),
         b_attn2(param.vector(format("h.{}.attn.c_proj.bias", b))),
-        w_mlp1(param.matrix(format("h.{}.mlp.c_fc.weight", b))),
+        w_mlp1(param.matrix(format("h.{}.mlp.c_fc.weight", b)).transpose()),
         b_mlp1(param.vector(format("h.{}.mlp.c_fc.bias", b))),
-        w_mlp2(param.matrix(format("h.{}.mlp.c_proj.weight", b))),
+        w_mlp2(param.matrix(format("h.{}.mlp.c_proj.weight", b)).transpose()),
         b_mlp2(param.vector(format("h.{}.mlp.c_proj.bias", b))) {
-    b_attn1 += param.vector(format("h.{}.ln_1.bias", b)).transpose() * w_attn1;
-    w_attn1.array().colwise() *=
-        param.vector(format("h.{}.ln_1.weight", b)).array() * sqrt(n_embd);
-    b_mlp1 += param.vector(format("h.{}.ln_2.bias", b)).transpose() * w_mlp1;
-    w_mlp1.array().colwise() *=
-        param.vector(format("h.{}.ln_2.weight", b)).array() * sqrt(n_embd);
+    b_attn1 += w_attn1 * param.vector(format("h.{}.ln_1.bias", b));
+    w_attn1.array().rowwise() *=
+        param.vector(format("h.{}.ln_1.weight", b)).array().transpose() * sqrt(n_embd);
+    b_mlp1 += w_mlp1 * param.vector(format("h.{}.ln_2.bias", b));
+    w_mlp1.array().rowwise() *=
+        param.vector(format("h.{}.ln_2.weight", b)).array().transpose() * sqrt(n_embd);
   }
 
   void operator()(Vector<float, n_embd> &x) {
     auto n_seq = kv.rows();
-    Vector<float, 3 * n_embd> qkv_x =
-        w_attn1.transpose() * (x.array() - x.mean()).matrix().normalized() +
-        b_attn1;
+    Vector<float, 3 *n_embd> qkv_x = w_attn1 * norm(x) + b_attn1;
     kv.conservativeResize(n_seq + 1, NoChange);
     kv.row(n_seq) = qkv_x.segment(n_embd, 2 * n_embd);
     Vector<float, n_embd> attn;
@@ -66,11 +64,13 @@ public:
       a /= a.array().sum();
       attn.segment(D * i, D) = v.transpose() * a;
     }
-    x += w_attn2.transpose() * attn + b_attn2;
-    Vector<float, 4 * n_embd> h =
-        w_mlp1.transpose() * (x.array() - x.mean()).matrix().normalized() +
-        b_mlp1;
+    x += w_attn2 * attn + b_attn2;
+    Vector<float, 4 *n_embd> h = w_mlp1 * norm(x) + b_mlp1;
     h.array() *= (1 + (h / sqrt(2)).array().erf()) / 2;
-    x += w_mlp2.transpose() * h + b_mlp2;
+    x += w_mlp2 * h + b_mlp2;
+  }
+
+  static Vector<float, n_embd> norm(const Vector<float, n_embd> &x) {
+    return (x.array() - x.mean()).matrix().normalized();
   }
 };
