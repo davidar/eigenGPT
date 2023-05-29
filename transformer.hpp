72c0ccd7394bda7d34f345afb55137940875b685
David A Roberts <d@vidr.cc>
2023-05-29 12:02:01 +0000

    macro

--- transformer.hpp
+++ transformer.hpp
@@ -11,6 +11,8 @@ constexpr size_t n_layer = 12;
 constexpr size_t n_vocab = 50257;
 constexpr size_t D = n_embd / n_head;
 
+#define FOR_EMBED(var, mul) for (int var = 0; var < mul * n_embd; var++)
+
 class TransformerBlock {
 public:
   float kv[n_ctx][2 * n_embd];
@@ -33,18 +35,13 @@ public:
         w_ln2(param.data(format("h.{}.ln_2.weight", b))) {}
 
   static void norm(float x[n_embd]) {
-    float sum = 0;
-    for (int i = 0; i < n_embd; i++)
-      sum += x[i];
-    float mean = sum / n_embd;
-    for (int i = 0; i < n_embd; i++)
-      x[i] -= mean;
+    float mean = 0;
+    FOR_EMBED(i, 1) mean += x[i] / n_embd;
+    FOR_EMBED(i, 1) x[i] -= mean;
     float norm = 0;
-    for (int i = 0; i < n_embd; i++)
-      norm += x[i] * x[i];
+    FOR_EMBED(i, 1) norm += x[i] * x[i];
     norm = sqrt(norm);
-    for (int i = 0; i < n_embd; i++)
-      x[i] /= norm;
+    FOR_EMBED(i, 1) x[i] /= norm;
   }
 
   void operator()(float x[n_embd]) {
@@ -53,17 +50,17 @@ public:
     norm(norm_x);
 
     // update kv cache
-    float qkv_x[3 * n_embd] = {0};
-    for (int i = 0; i < 3 * n_embd; i++) {
-      for (int j = 0; j < n_embd; j++) {
+    float qkv_x[3 * n_embd];
+    FOR_EMBED(i, 3) {
+      qkv_x[i] = b_attn1[i];
+      FOR_EMBED(j, 1) {
         qkv_x[i] += w_attn1[j * (3 * n_embd) + i] *
                     (b_ln1[j] + w_ln1[j] * sqrt(n_embd) * norm_x[j]);
       }
-      qkv_x[i] += b_attn1[i];
       if (i >= n_embd)
         kv[n_seq][i - n_embd] = qkv_x[i];
     }
-    n_seq += 1;
+    n_seq++;
 
     // attention
     float attn[n_embd] = {0};
@@ -71,42 +68,38 @@ public:
     for (int head = 0; head < n_head; head++) {
       for (int posn = 0; posn < n_seq; posn++) {
         float a = 0;
-        for (int d = 0; d < D; d++) {
-          a += kv[posn][d + D * head] * qkv_x[d + D * head];
+        for (int d = D * head; d < D + D * head; d++) {
+          a += kv[posn][d] * qkv_x[d];
         }
         a = std::exp(a / sqrt(D));
-        for (int d = 0; d < D; d++) {
-          attn[d + D * head] += kv[posn][d + D * head + n_embd] * a;
+        for (int d = D * head; d < D + D * head; d++) {
+          attn[d] += kv[posn][d + n_embd] * a;
         }
         asum[head] += a;
       }
     }
 
-    for (int i = 0; i < n_embd; i++) {
-      for (int j = 0; j < n_embd; j++) {
-        x[i] += w_attn2[j * n_embd + i] * attn[j] / asum[j / D];
-      }
+    FOR_EMBED(i, 1) {
+      FOR_EMBED(j, 1) x[i] += w_attn2[j * n_embd + i] * attn[j] / asum[j / D];
       x[i] += b_attn2[i];
     }
 
     // mlp
-    float h[4 * n_embd] = {0};
+    float h[4 * n_embd];
     memcpy(norm_x, x, sizeof(float) * n_embd);
     norm(norm_x);
 
-    for (int i = 0; i < 4 * n_embd; i++) {
-      for (int j = 0; j < n_embd; j++) {
+    FOR_EMBED(i, 4) {
+      h[i] = b_mlp1[i];
+      FOR_EMBED(j, 1) {
         h[i] += w_mlp1[j * (4 * n_embd) + i] *
                 (b_ln2[j] + w_ln2[j] * sqrt(n_embd) * norm_x[j]);
       }
-      h[i] += b_mlp1[i];
       h[i] *= (1 + std::erf(h[i] / sqrt(2))) / 2;
     }
 
-    for (int i = 0; i < n_embd; i++) {
-      for (int j = 0; j < 4 * n_embd; j++) {
-        x[i] += w_mlp2[j * n_embd + i] * h[j];
-      }
+    FOR_EMBED(i, 1) {
+      FOR_EMBED(j, 4) x[i] += w_mlp2[j * n_embd + i] * h[j];
       x[i] += b_mlp2[i];
     }
   }
