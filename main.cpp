6086d0d447f094f8f9f42dffcfdbe3aff1dca857
David A Roberts <d@vidr.cc>
2023-05-31 08:23:55 +0000

    empty prompt

--- main.cpp
+++ main.cpp
@@ -38,18 +38,13 @@ int main() {
   }
   */
 
-  std::vector<int> prompt =
-      tokeniser("Alan Turing theorized that computers would one day become");
-  int n_tokens_to_generate = 40;
-  std::vector<int> tokens = prompt;
-  int total = tokens.size() + n_tokens_to_generate;
-  assert(total < n_ctx);
-  for (int posn = 0; posn < total; posn++) {
-    int token = tokens[posn];
-    std::cerr << vocab[token] << std::flush;
+  int seed = time(NULL);
+  std::cerr << "seed: " << seed << std::endl;
+  srand(seed);
+
+  int token = n_vocab - 1;
+  for (int posn = 0; posn < n_ctx; posn++) {
     token = transformer(token, posn);
-    if (posn + 1 >= tokens.size()) {
-      tokens.push_back(token);
-    }
+    std::cerr << vocab[token] << std::flush;
   }
 }
